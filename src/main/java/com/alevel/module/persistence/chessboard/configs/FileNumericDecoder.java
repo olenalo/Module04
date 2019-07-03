c7067484e53e2183fe7b47ccb2fb397958d7ef0a
olenalo <uleta@ukr.net>
2019-07-03 22:02:41 +0300

    Add Rank and File enums numeric decoders

---
+++ src/main/java/com/alevel/module/persistence/chessboard/configs/FileNumericDecoder.java
@@ -0,0 +1,30 @@
+package com.alevel.module.persistence.chessboard.configs;
+
+import com.google.common.collect.ImmutableMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.alevel.module.persistence.chessboard.configs.File.A;
+import static com.alevel.module.persistence.chessboard.configs.File.B;
+import static com.alevel.module.persistence.chessboard.configs.File.C;
+import static com.alevel.module.persistence.chessboard.configs.File.D;
+import static com.alevel.module.persistence.chessboard.configs.File.E;
+import static com.alevel.module.persistence.chessboard.configs.File.F;
+import static com.alevel.module.persistence.chessboard.configs.File.G;
+import static com.alevel.module.persistence.chessboard.configs.File.H;
+
+
+public class FileNumericDecoder {
+
+    public static Map<File, Integer> FILE_NUMERIC_DECODER = ImmutableMap.<File, Integer>builder()
+            .put(A, 1)
+            .put(B, 2)
+            .put(C, 3)
+            .put(D, 4)
+            .put(E, 5)
+            .put(F, 6)
+            .put(G, 7)
+            .put(H, 8)
+            .build();
+}
---
+++ src/main/java/com/alevel/module/persistence/chessboard/configs/RankNumericDecoder.java
@@ -0,0 +1,28 @@
+package com.alevel.module.persistence.chessboard.configs;
+
+import com.google.common.collect.ImmutableMap;
+
+import java.util.Map;
+
+import static com.alevel.module.persistence.chessboard.configs.Rank.ONE;
+import static com.alevel.module.persistence.chessboard.configs.Rank.TWO;
+import static com.alevel.module.persistence.chessboard.configs.Rank.THREE;
+import static com.alevel.module.persistence.chessboard.configs.Rank.FOUR;
+import static com.alevel.module.persistence.chessboard.configs.Rank.FIVE;
+import static com.alevel.module.persistence.chessboard.configs.Rank.SIX;
+import static com.alevel.module.persistence.chessboard.configs.Rank.SEVEN;
+import static com.alevel.module.persistence.chessboard.configs.Rank.EIGHT;
+
+public class RankNumericDecoder {
+
+    public static Map<Rank, Integer> FILE_NUMERIC_DECODER = ImmutableMap.<Rank, Integer>builder()
+            .put(ONE, 1)
+            .put(TWO, 2)
+            .put(THREE, 3)
+            .put(FOUR, 4)
+            .put(FIVE, 5)
+            .put(SIX, 6)
+            .put(SEVEN, 7)
+            .put(EIGHT, 8)
+            .build();
+}
